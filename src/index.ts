c3431aad21fa5aafc35c86d9589f4f794ea498d6
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2023-02-22 16:38:46 -0600

    Node section ended

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,22 @@
 //2.-ejecutar tsc --init
 import express from 'express';
 
+type Usuario = {
+  id: string;
+  name: string;
+};
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.status(200).json({ message: 'hello mundo' });
+});
+
+app.post('/', (req, res) => {
+  const { name } = req.body as Usuario;
+  console.log(name);
+  res.send(`ok`);
 });
 
 app.listen(4000, () => {
